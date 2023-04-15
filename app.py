a8a483fb0c66441ac50a16def028349b60c28d6f
Priyesh <priyesh.sriv2017@gmail.com>
2023-04-15 08:25:19 +0000

    Setup Commit

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, make_response
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+class PortfolioOrders(Resource):
+
+    def get(self):
+        # Return holdings data 
+        pass
+
+    def post(self):
+        # Set API secret/API Key
+        pass
+
+
+class KnowledgeBase(Resource):
+    def get(self):
+        # This is the get call for financial knowledge base
+
+
+class ChatResponse(Resource):
+    def get(self):
+        # Bot Response
+
+
+class ProductCatalog(Resource):
+    def get(self):
+        # Fetch from product catalog
+    def post(self):
+        # Set a product catalog
+
+
+api.add_resource(PortfolioOrders, "/portfolio")
+api.add_resource(KnowledgeBase, "/knowledgebase")
+api.add_resource(ChatResponse, "/bot")
+api.add_resource(ProductCatalog, "/products")
